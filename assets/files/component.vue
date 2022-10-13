ff747335a9d1302c439aa8f3e000b7039b260d9a
Sil van Diepen <me@sil.mt>
2022-10-13 10:03:32 +0200

    add affix of type

--- assets/files/component.vue
+++ assets/files/component.vue
@@ -1,3 +1,3 @@
-<template src="./COMPONENT_NAME.html"></template>
-<script lang="ts" src="./COMPONENT_NAME.ts"></script>
-<style lang="scss" src="./COMPONENT_NAME.scss"></style>
+<template src="./COMPONENT_NAME.template.html"></template>
+<script lang="ts" src="./COMPONENT_NAME.script.ts"></script>
+<style lang="scss" src="./COMPONENT_NAME.style.scss"></style>
